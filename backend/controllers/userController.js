26d0e5e94eb1a689508ef86dc5d67846e72f2f6a
sheth-jay <87015320+sheth-jay@users.noreply.github.com>
2023-07-11 15:40:26 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+  console.log(">>>user, user);
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
