150d5379dcea1078f1b0e5452de83f3a79776cf9
sheth-jay <87015320+sheth-jay@users.noreply.github.com>
2023-07-11 16:47:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,7 @@ app.use(function (req, res, next) {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
-
- 
-
+  
   next();
 });
 
