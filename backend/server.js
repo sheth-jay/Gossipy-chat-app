e2a44ac731c48cb9c2ec495aec4be79a8c2919fc
sheth-jay <87015320+sheth-jay@users.noreply.github.com>
2023-07-11 16:47:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
